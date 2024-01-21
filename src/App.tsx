7a15a6a071105e37e0ebe06276f0370775fa4f2c
Jeroen Leijnse <jr_leijnse@live.nl>
2024-01-21 21:16:42 +0100

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import Input from "./components/Input";
+import Button from "./components/Button";
+import Container from "./components/Container";
 
 function App() {
+  function doSomething() {
+    console.log("hi");
+  }
+
   return (
     <main>
       <Input id="name" type="text" label="Your name" />
       <Input id="age" type="number" label="Your age" />
+      <p>
+        <Button>A Button</Button>
+      </p>
+      <p>
+        <Button href="https://google.com">A Link</Button>
+      </p>
+      <Container onClick={doSomething} as={"button"}>
+        Click me
+      </Container>
     </main>
   );
 }
