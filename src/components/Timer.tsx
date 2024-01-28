a9b4f42fffdefbd6ea26c6e3a9158f14ca6c1b39
Jeroen Leijnse <jr_leijnse@live.nl>
2024-01-28 19:16:03 +0100

    Update Timer.tsx

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,28 @@
 import Container from "./UI/Container.tsx";
 import { type Timer as TimerProps } from "../store/timers-context.tsx";
+import { useEffect, useState } from "react";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRemainingTime((prevTime) => {
+        return prevTime > 0 ? prevTime - 50 : 0;
+      });
+    }, 50);
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedRemaingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemaingTime}</p>
     </Container>
   );
 }
